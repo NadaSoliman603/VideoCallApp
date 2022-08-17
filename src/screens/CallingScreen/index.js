b11233c17c2ab4edc41ddaa15bdc1d0043e6e14d
NadaSoliman603 <72712723+NadaSoliman603@users.noreply.github.com>
2022-08-17 11:15:37 +0200

    UI screens

---
+++ src/screens/CallingScreen/index.js
@@ -0,0 +1,98 @@
+import React, {useEffect, useState, useRef} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  PermissionsAndroid,
+  Alert,
+  Platform,
+} from 'react-native';
+import CallActionBox from '../../components/CallActions';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+function CallingScreen() {
+    const [permissionGranted, setPermissionGranted] = useState(false);
+    const [callStatus, setCallStatus] = useState('Initializing...');
+    const [localVideoStreamId, setLocalVideoStreamId] = useState('');
+    const [remoteVideoStreamId, setRemoteVideoStreamId] = useState('');
+
+    return ( 
+        <View style={styles.page}>
+        <Pressable onPress={()=>{}} style={styles.backButton}>
+          <Ionicons name="chevron-back" color="white" size={35} />
+        </Pressable>
+  
+        {/* <Voximplant.VideoView
+          videoStreamId={remoteVideoStreamId}
+          style={styles.remoteVideo}
+        />
+  
+        <Voximplant.VideoView
+          videoStreamId={localVideoStreamId}
+          style={styles.localVideo}
+        />
+   */}
+        <View style={styles.cameraPreview}>
+          <Text style={styles.name}>{"user?.user_display_name"}</Text>
+          <Text style={styles.phoneNumber}>{callStatus}</Text>
+        </View>
+  
+        <CallActionBox onHangupPress={()=>{}} />
+      </View>
+     );
+}
+
+
+const styles = StyleSheet.create({
+    page: {
+      height: '100%',
+      backgroundColor: '#7b4e80',
+    },
+    cameraPreview: {
+      flex: 1,
+      alignItems: 'center',
+      paddingTop: 10,
+      paddingHorizontal: 10,
+    },
+    localVideo: {
+      width: 100,
+      height: 150,
+      backgroundColor: '#ffff6e',
+  
+      borderRadius: 10,
+  
+      position: 'absolute',
+      right: 10,
+      top: 100,
+    },
+    remoteVideo: {
+      backgroundColor: '#7b4e80',
+      borderRadius: 10,
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 100,
+    },
+    name: {
+      fontSize: 30,
+      fontWeight: 'bold',
+      color: 'white',
+      marginTop: 50,
+      marginBottom: 15,
+    },
+    phoneNumber: {
+      fontSize: 20,
+      color: 'white',
+    },
+    backButton: {
+      position: 'absolute',
+      top: 20,
+      left: 10,
+      zIndex: 10,
+    },
+  });
+
+export default CallingScreen;

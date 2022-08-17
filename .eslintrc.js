2072b52b10f5ee953ccb60769951fbfc96701acd
NadaSoliman603 <72712723+NadaSoliman603@users.noreply.github.com>
2022-08-17 11:14:49 +0200

    ui screens

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+
+  rules: {
+    'prettier/prettier': 0,
+  },
+  
+eslint: {
+  configure: {
+    rules: {
+      // "no-unused-vars": "off"
+    }
+  }
+}
 };
+
